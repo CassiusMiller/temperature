3e381234ef23f1ee5764a0c84face2b2a6122fc5
CassiusMiller <cassius.miller@sdgku.edu>
2025-01-23 15:33:40 -0800

    version1

---
+++ scripts/script.js
@@ -0,0 +1,90 @@
+console.log("Script");
+//keyword nameVar = value;
+//key words you can use are (var, let, const)
+
+//string
+var firstName = "cassius";
+var lastName = "miller";
+
+// numerical values
+let myAge= 30;
+let myHeight= 74;
+
+
+//boolean (true / false)
+let isStudent = true;
+
+
+// display the values on the console
+console.log("My name is: " + firstName,lastName + "I am" + myAge + "years old, my size is" + myHeight );
+console.log(isStudent);
+
+//arithmetic operations
+let num1=10;
+let num2=2;
+
+let sum = num1 + num2;
+let sub = num1 - num2
+let mul = num1 * num2
+let div = num1 / num2
+
+
+
+
+console.log("The sum is:" + sum)
+console.log("The sub is:" + sub)
+console.log("The mul is:" + mul)
+console.log("The div is:" + div)
+
+
+
+//challange 1
+
+let area = 0;
+const pi = 3.1416;
+let radius = prompt("enter radius");
+
+
+area = pi * radius * radius
+
+radius=5
+
+console.log("this area of the circle with a radius of" + radius + "is the" + area)
+
+
+
+
+integerNumber = 1625;
+
+console.log("the value of integernumber is" + integerNumber);
+
+integerNumber = 375;
+
+alert(integerNumber + 125);
+
+age = prompt("user's age")
+alert("thank you for your answer")
+console.log(age)
+alert("you are " + age + " years old")
+
+
+//definition a variable
+let aVariable;
+
+//assigning a value
+let aVarible = 20;
+
+//define and assiing the value in the same line
+let otherVariable = 10;
+
+console.log(aVariable,otherVariable)
+
+aVariable=50
+otherVariable=40
+
+console.log(aVariable,otherVariable)
+
+const weekDays = 7
+console.log(weekDays)
+
+//the constant(const) is always the same.
