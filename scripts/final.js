9151da453d731b0b759a69c76c2c39b19cda8779
CassiusMiller <cassius.miller@sdgku.edu>
2025-02-01 15:07:58 -0800

    final updated

--- scripts/final.js
+++ scripts/final.js
@@ -9,10 +9,10 @@ function convert(){
     for (let i = start; i <= end; i++) {
         if (scale == "C") {
             convertedTemp = (i * 9/5) + 32; 
-            p.innerHTML=(`${i}Celsius is ${convertedTemp}Fahrenheit`);
+            p.innerHTML+=(`<p> ${i}Celsius is ${convertedTemp}Fahrenheit </p>`);
         }else if (scale == "F") {
             convertedTemp = (i - 32) * 5/9;
-            p.innerHTML=(`${i}Fahrenheit is ${convertedTemp}Celsius`);
+            p.innerHTML+=(`<p> ${i}Fahrenheit is ${convertedTemp}Celsius </p>`);
         } else {
             p.innerHTML=("Invalid scale. Please use 'C' for Celsius to Fahrenheit or 'F' for Fahrenheit to Celsius):");
         }
